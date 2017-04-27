24bfa32c052a3350d6c6a088c1a30679a2c73a5f
imininnik <evg.bakhmin@gmail.com>
2017-04-27 22:05:17 +0300

    Controls?

---
+++ Assets/PixelTexturePack/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        gameObject.GetComponent<Transform>().Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (Input.GetButtonDown("E")) {
+            //do something
+        }
+    }
+}
