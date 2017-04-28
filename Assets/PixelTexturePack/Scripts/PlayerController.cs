6edbd30a4934e7a278960c12b1a5f6873df50cf5
imininnik <evg.bakhmin@yandex.ru>
2017-04-28 14:54:10 +0300

    Controls[2]

--- Assets/PixelTexturePack/Scripts/PlayerController.cs
+++ Assets/PixelTexturePack/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        gameObject.GetComponent<Transform>().Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        gameObject.GetComponent<Transform>().Translate(Input.GetAxis("Horizontal")*speed, 0, Input.GetAxis("Vertical")*speed);
         if (Input.GetButtonDown("E")) {
             //do something
         }
