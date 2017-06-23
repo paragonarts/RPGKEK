e99d5aca311a82b33552a596ccf3ba439e54f6b2
imininnik <evg.bakhmin@gmail.com>
2017-06-23 18:25:45 +0300

    Controls

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject Player;
+    public bool Rotating;
+    // Use this for initialization
+    Vector3 Offset;
+    void Start () {
+        Offset = new Vector3(-24.64f, 17.66f, -26.87f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Rotating)
+        {
+            Vector3 PlayerPosition = Player.GetComponent<Transform>().position;
+            gameObject.GetComponent<Transform>().position = PlayerPosition + Offset;
+        }
+    }
+    public void ChangeOffset(float Axis)
+    {
+        Offset = new Vector3(Offset.z * Axis, 17.66f, Offset.x * Axis * (-1));
+    }
+
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,25 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public float speed;
-//  public delegate void ActionListeners();
-//  public event ActionListeners OnPlayersAction;
+    public Vector3 CameraCoords;
+    public bool IsRotating = false;
+    //  public delegate void ActionListeners();
+    //  public event ActionListeners OnPlayersAction;
     public GameObject CameraRotator;
+    public Ray r;
+    Vector3 hited;
+    public Vector3 jopa;
+    public CameraController CameraScript;
     // Use this for initialization
-    void Start () {
-        
+    void Start()
+    {
+        CameraScript = CameraRotator.GetComponent<CameraController>();
     }
 
     // Update is called once per frame
-    void Update () {
-        gameObject.GetComponent<Transform>().Translate(Input.GetAxis("Horizontal")*speed, 0, Input.GetAxis("Vertical")*speed);
+    void Update()
+    {
+        //Vector3 jopa = new Vector3(Input.mousePosition.x, 0f, Input.mousePosition.y);
+        r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-        if (Input.GetButtonDown("CameraRotation"))
+        if (Physics.Raycast(r, out hit))
         {
-            Debug.Log(Input.GetAxis("CameraRotation"));
-            CameraRotator.GetComponent<Transform>().RotateAround(gameObject.GetComponent<Transform>().position, Vector3.up, Input.GetAxis("CameraRotation") * 90);
+            hited = hit.point;
+        }
+        else { hited = Vector3.zero; }
+        Debug.Log(hited);
+        jopa = new Vector3(hited.x, 1, hited.z);
+        gameObject.GetComponent<Transform>().LookAt(jopa);
+        //gameObject.GetComponent<Transform>().LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        gameObject.GetComponent<Transform>().Translate(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+
+        if (Input.GetButtonDown("CameraRotation") && !IsRotating)
+        {
+            IsRotating = true;
+            CameraScript.Rotating = true;
+            CameraCoords.x = CameraRotator.GetComponent<Transform>().localPosition.x;
+            CameraCoords.y = CameraRotator.GetComponent<Transform>().eulerAngles.y;
+            CameraCoords.z = CameraRotator.GetComponent<Transform>().localPosition.z;
+            StartCoroutine(CameraRotate(Input.GetAxis("CameraRotation"), 0f));
+            CameraScript.ChangeOffset(Input.GetAxis("CameraRotation"));
+
 
         }
+    }
+    IEnumerator CameraRotate(float Axis, float angle)
+    {
+
+        while (angle <= 90)
+        {
+            CameraRotator.GetComponent<Transform>().RotateAround(gameObject.GetComponent<Transform>().position, Vector3.up, Axis * 90 * Time.deltaTime);
+            angle += 90 * Time.deltaTime;
+            yield return null;
+        }
+        CheckCoords(Axis);
+        IsRotating = false;
+        CameraScript.Rotating = false;
+        yield break;
+    }
+    private void CheckCoords(float axis)
+    {
+        //CameraRotator.GetComponent<Transform>().localPosition = new Vector3(CameraCoords.z * axis, CameraRotator.GetComponent<Transform>().localPosition.y, CameraCoords.x * axis * (-1));
+        //CameraRotator.GetComponent<Transform>().rotation = Quaternion.Euler(30, CameraCoords.y + 90 * axis, 0);
     }
 }
